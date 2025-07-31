d87975f106353387aae8688966ee1145ca967178
Faizan-Khambre <163112231+Faizan-Khambre@users.noreply.github.com>
2025-08-01 00:51:10 +0530

    Updated employees.sql file

--- employees.sql
+++ employees.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `employee_system`
 --
-
+CREATE DATABASE employee_system;
+USE employee_system;
 -- --------------------------------------------------------
 
 --
